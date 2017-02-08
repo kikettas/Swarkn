ea2d381e775c5e4d3cb9ab6e5874da30c2d673c3
Enrique del Pozo Gómez <enrique0703@gmail.com>
2017-02-08 16:29:00 +0100

    - Added EnumCollection extension

--- Swarkn.podspec
+++ Swarkn.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'Swarkn'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'A Swiss Army Knife for your Swift project.'
 
 # This description is used to generate tags and improve search results.
---
+++ Swarkn/Classes/EnumCollection.swift
@@ -0,0 +1,25 @@
+//
+//  EnumCollection.swift
+//  Pods
+//
+//  Created by Enrique del Pozo Gómez on 2/8/17.
+//
+//
+
+import Foundation
+
+protocol EnumCollection : Hashable {}
+extension EnumCollection {
+    static func cases() -> AnySequence<Self> {
+        typealias S = Self
+        return AnySequence { () -> AnyIterator<S> in
+            var raw = 0
+            return AnyIterator {
+                let current : Self = withUnsafePointer(to: &raw) { $0.withMemoryRebound(to: S.self, capacity: 1) { $0.pointee } }
+                guard current.hashValue == raw else { return nil }
+                raw += 1
+                return current
+            }
+        }
+    }
+}
